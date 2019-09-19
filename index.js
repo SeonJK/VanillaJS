e4e8f617c7e5f614917cb9c72e0edfebde6a4946
tjswhdrbs14 <tjswhdrbs14@naver.com>
2019-09-19 16:08:01 +0900

    add js and use array/object

---
+++ index.js
@@ -0,0 +1,13 @@
+const nicoInfo = { 
+    naem: "Nicolas",
+    age: 33,
+    gender: "Male",
+    isHandsome:true,
+    facMovies: ["Along the gods", "LOTR", "Oldboy"],
+    favFood: [
+        {name: "Kimchi", fatty: false}, 
+        {name: "Cheese burger", fatty: true}
+    ]
+}
+
+console.log(nicoInfo);
