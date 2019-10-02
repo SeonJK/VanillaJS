92ed0fbfddb55b5fac89c8f16e21387f43b565e2
tjswhdrbs14 <tjswhdrbs14@naver.com>
2019-10-02 12:03:11 +0900

    use if-else statement and exercise DOM

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const nicoInfo = { 
-    naem: "Nicolas",
-    age: 33,
-    gender: "Male",
-    isHandsome:true,
-    facMovies: ["Along the gods", "LOTR", "Oldboy"],
-    favFood: [
-        {name: "Kimchi", fatty: false}, 
-        {name: "Cheese burger", fatty: true}
-    ]
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(27, 135, 207)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick(event){
+    const currentColor = title.style.color;
+    console.log(currentColor);
+
+    if(currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    }else{
+        title.style.color = BASE_COLOR;
+    }
 }
 
-console.log(nicoInfo);
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("mouseenter", handleClick);
+}
+
+init();
