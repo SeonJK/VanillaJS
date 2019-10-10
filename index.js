33fdeaabd77d6574d1bd963fe0c7d649512be209
tjswhdrbs14 <tjswhdrbs14@naver.com>
2019-10-10 16:07:26 +0900

    use DOM especially className and classList

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(27, 135, 207)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
 
-function handleClick(event){
-    const currentColor = title.style.color;
-    console.log(currentColor);
-
-    if(currentColor === BASE_COLOR){
-        title.style.color = OTHER_COLOR;
-    }else{
-        title.style.color = BASE_COLOR;
-    }
+function handleClick() {
+    // const currentClass = title.className;
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    // if(!hasClass){
+    //     title.classList.add(CLICKED_CLASS);
+    // }else{
+    //     title.classList.remove(CLICKED_CLASS);
+    // }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init(){
-    title.style.color = BASE_COLOR;
-    title.addEventListener("mouseenter", handleClick);
+    title.addEventListener("click", handleClick);
 }
 
 init();
